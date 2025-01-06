d71ab395e0d5e51661dc21bfb566328999c22ede
AlexisFlo <floresfernandezalexishr@gmail.com>
2025-01-05 23:46:32 -0600

    add info about edadismo

---
+++ src/utils/case.ts
@@ -0,0 +1,20 @@
+const cases = [
+  {
+    title:"Promoción de la educación intergeneracional",
+    description:"Facilitar espacios donde personas de diferentes edades puedan aprender unas de otras."
+  },
+  {
+    title:"Campañas de concienciación",
+    description:"Difundir información que desafíe los mitos y prejuicios sobre las personas mayores."
+  },
+  {
+    title:"Empoderamiento de las personas mayores",
+    description:"Fomentar su participación activa en la sociedad, reconociendo su valor y contribución."
+  },
+  {
+    title:"Atención médica sin sesgos",
+    description:"Capacitar a los profesionales de la salud para evitar actitudes edadistas en su práctica."
+  },
+]
+
+export { cases }
