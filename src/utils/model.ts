e2d432f647974b61e504efb507af6618eeab3082
AlexisFlo <floresfernandezalexishr@gmail.com>
2025-01-06 19:14:30 -0600

    add models section

---
+++ src/utils/model.ts
@@ -0,0 +1,16 @@
+const models = [
+  {
+    title: "Da ejemplo",
+    description: "Muestra respeto, empatía y aprecio hacia las personas mayores en tu vida diaria. Otros seguirán tu ejemplo."
+  },
+  {
+    title: "Inspira a otros",
+    description: "Comparte tus esfuerzos por combatir el edadismo en redes sociales, comunidades y conversaciones cotidianas."
+  },
+  {
+    title: "Reconoce su valor",
+    description: "Resalta las contribuciones de las personas mayores a la sociedad, desde su sabiduría hasta su experiencia profesional y personal."
+  },
+]
+
+export { models }
